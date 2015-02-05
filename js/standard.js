782b6aedfd71b5a879b9598b932aaa2df2f684d4
mvitalia <p.guglielmo@mvitalia.com>
2015-02-05 17:09:55 +0100

    o

--- js/standard.js
+++ js/standard.js
@@ -1,22 +1,53 @@
 $(window).load(function(){
-$('#search').keyup(function(){
-	var searchField = $('#search').val();
-	var regex = new RegExp(searchField, "i");
-	var output = '<div class="row"><ul>';
-	var count = 1;
-	$.getJSON("http://www.mvitalia.com/dimostrativi/patrizia/portaleapp/admin/get_json_data.asp", function(data) {
-	  $.each(data, function(key, val){
-		if ((val.ragione_sociale.search(regex) != -1) || (val.categoria.search(regex) != -1)) {
-		  output += "<li id='" + val.ID + "'><a data-transition='pop' href='page/dettaglio.html?id=" + val.ID + "'><img src=''>" + val.ragione_sociale + "</a></li>";
-		  
-		  if(count%2 == 0){
-			output += '</div><div class="row">'
-		  }
-		  count++;
-		}
-	  });
-	  output += '</ul></div>';
-	  $('#results').html(output);
-	}); 
+	$('#search').keyup(function(){
+		var searchField = $('#search').val();
+		var regex = new RegExp(searchField, "i");
+		var output = '<div class="row"><ul>';
+		var count = 1;
+		$.getJSON("http://www.mvitalia.com/dimostrativi/patrizia/portaleapp/admin/get_json_data.asp", function(data) {
+		  $.each(data, function(key, val){
+			if ((val.ragione_sociale.search(regex) != -1) || (val.categoria.search(regex) != -1)) {
+			  output += "<li id='" + val.ID + "'><a data-transition='pop' href='page/dettaglio.html?id=" + val.ID + "'><img src=''>" + val.ragione_sociale + "</a></li>";
+			  
+			  if(count%2 == 0){
+				output += '</div><div class="row">'
+			  }
+			  count++;
+			}
+		  });
+		  output += '</ul></div>';
+		  $('#results').html(output);
+		}); 
+	});
 });
-});
+
+
+
+
+function parseGetVars()
+{
+  // creo una array
+  var args = new Array();
+  // individuo la query (cioè tutto quello che sta a destra del ?)
+  // per farlo uso il metodo substring della proprietà search
+  // dell'oggetto location
+  var query = window.location.search.substring(1);
+  // se c'è una querystring procedo alla sua analisi
+  if (query)
+  {
+    // divido la querystring in blocchi sulla base del carattere &
+    // (il carattere & è usato per concatenare i diversi parametri della URL)
+    var strList = query.split('&');
+    // faccio un ciclo per leggere i blocchi individuati nella querystring
+    for(str in strList)
+    {
+      // divido ogni blocco mediante il simbolo uguale
+      // (uguale è usato per l'assegnazione del valore)
+      var parts = strList[str].split('=');
+      // inserisco nella array args l'accoppiata nome = valore di ciascun
+      // parametro presente nella querystring
+      args[unescape(parts[0])] = unescape(parts[1]);
+    }
+  }
+  return args;
+}
